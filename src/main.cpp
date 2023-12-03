b1949a62390cbb1d4600eea46dd42b5d1dd0594b
unsplorer <omnitk@gmail.com>
2023-12-03 12:00:26 -0600

    add encoder gpios

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,19 @@
 #include <WiFiManager.h>
 #include <WiFiUdp.h>
 
+#define PUSH_BUTTON         0           // Encoder pushbutton
+#define ENCODER_LEFT        5           // These maybe backwards, need to test
+#define ENDCODE_RIGHT       4           // ^^
 #define DATA_PIN            13          // MTCK - maybe the engineers have it backwards, or maybe I do! Logic analyzer will tell
 #define CLOCK_PIN           14          // MTMS - Master Transmit Master Select - this is normally the data output
 #define MTDO                15          // Normally SPI Data Out -> this is connected to LCD pin 12
 #define MTDI                12          // Normally SPI Data IN  -> this is conencted to LCD pin 14
 #define BIT_ORDER           LSBFIRST    // littleEndian
 
-
+/*
+  TODO:
+    ADD ENCODER FUNCTIONALITY
+*/
 void setupOTA() {
   ArduinoOTA.onStart([]() {});
   ArduinoOTA.onEnd([]() {});
